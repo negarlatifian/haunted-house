5274b1036bd5eb099dd0f0c62d655cc2cd5a546a
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-01-19 16:21:23 +0100

    fixed deployment issue

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
-import { random } from 'supervillains';
 
 /**
  * Base
