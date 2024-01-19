890566c6370668fa2ef7bd6f0ac28089a33c4f05
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-01-19 16:15:34 +0100

    optimized the shadowMaps

--- src/script.js
+++ src/script.js
@@ -264,7 +264,9 @@ renderer.setClearColor('#262837');
  * Shadows
  */
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 moonLight.castShadow = true;
+doorLight.castShadow = true;
 ghost1.castShadow = true;
 ghost2.castShadow = true;
 ghost3.castShadow = true;
@@ -276,6 +278,28 @@ bush3.castShadow = true;
 bush4.castShadow = true;
 
 floor.receiveShadow = true;
+
+// Optimize the shadowMaps
+
+moonLight.shadow.mapSize.width = 256;
+moonLight.shadow.mapSize.height = 256;
+moonLight.shadow.camera.far = 15;
+
+doorLight.shadow.mapSize.width = 256;
+doorLight.shadow.mapSize.height = 256;
+doorLight.shadow.camera.far = 7;
+
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.camera.far = 7;
+
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.far = 7;
+
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.far = 7;
 
 /**
  * Animate
